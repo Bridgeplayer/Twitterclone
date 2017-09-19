76a28d917c93ec4e1c53b62420371257eda68eb8
Pictionary <opcoys@hotmail.com>
2017-09-19 16:50:59 +0800

    Add Tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,36 @@
 class TweetsController < ApplicationController
 
+  	before_action :find_tweet, only: [:edit, :show, :update, :destroy]
+
+  	def find_tweet
+		@twee = Tweet.find(params[:id])
+  	end
+
 	def index
 		@twee = Tweet.all
 		@lik = Like.all
 	end
 
+	def new
+		@twee = Tweet.new
+	end
+
+	def create
+		# post_params = params[:tweet].permit(:post)
+		@twee = Tweet.new(post_params)
+
+		if @twee.save
+			flash[:success] = "Bookrecord successfully created"
+			redirect_to tweets_path
+		else
+			flash.now[:failure] = "Bookrecord not successfully created"
+			render "new"
+		end
+	end
+
+
+	private
+	def post_params
+		params.require(:tweet).permit(:post)
+	end
 end
