0bd5fc59b5acf7aa302c49c120d4c07a721ea86d
sarahjspd <sarahj.spd@gmail.com>
2017-09-20 16:28:05 +0800

    reply work now

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,12 +1,13 @@
 class RepliesController < ApplicationController
 
 	def create
-		@twee = Tweet.find_by(id: params[:id])
-		repli_params = params.require(:repli).permit(:post)
+		@twee = Tweet.find(params[:tweet_id])
+		repli_params = params.require(:reply).permit(:post)
 		@repli = @twee.replies.build(repli_params)
 		@repli.user_id = current_user.id
+		@repli.tweet_id = @twee.id
 		if @repli.save
-			flash[:notice] = "Commented Successfully"
+			flash[:notice] = "Replied Successfully"
 			redirect_to tweet_path(id: @twee.id)
 		else
 			flash.now[:notice] = "Unsuccessful, please check your input"
