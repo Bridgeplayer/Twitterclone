3ab8e77ce58903c4273bfd127a2e0a57d181d5bf
Pictionary <opcoys@hotmail.com>
2017-11-16 17:16:29 +0800

    uncomment tweets.js

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  # disable this two lines below for heroku to work
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.delivery_method = :letter_opener_web  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener_web  
 end
